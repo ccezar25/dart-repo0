e05d7e26f41f59fc83330ce83a6b3c737eaf0b6f
Caio Cezar <127907211+ccezar25@users.noreply.github.com>
2023-12-28 01:00:28 -0300

    Add files via upload

---
+++ program2.dart
@@ -0,0 +1,15 @@
+void main (){
+
+	List<dynamic>  v1 = ["", 25.5, true, "Jamal"];
+	List<String>  v2 = ["Everson", "Davi"];
+	List<String>  v3 = ["Maria"];
+	List<String>  v4 = ["João", "Carlos"];
+
+	print(v1);
+	print(v2);
+	print(v1.length);
+	print(v1[2]);
+	print("\n \n \n");
+	print("EndPoint");
+
+}
